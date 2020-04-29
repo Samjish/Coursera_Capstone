f108ba69cebafe815c4564e8a9cb939476f7255a
Samjish <38495593+Samjish@users.noreply.github.com>
2020-04-29 08:42:20 +0530

    Add files via upload

---
+++ IBM Applied DS Capstone.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #This project is part of IBM Coursera Applied DS capstone project
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
